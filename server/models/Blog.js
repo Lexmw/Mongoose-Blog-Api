7e6353c9b101c93e7022ab132eff818881a35bca
lexius <lexius.waltar@gmail.com>
2019-09-24 22:37:32 -0700

    circle ci

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -2,11 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const BlogSchema = new Schema({
+
     title:{type:String, require:true}, 
     article:{type: String, required:true}, 
     published:{type:Date, required:true},
     featured:{type:Boolean, required:true},
     author:{ type: Schema.Types.ObjectId, ref: 'User', required:true }
+    
 });
 
 module.exports = mongoose.model('Blog', BlogSchema);
